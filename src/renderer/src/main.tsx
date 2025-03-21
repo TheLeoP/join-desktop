3f50aa426eec980b2950969be933fe45ce0c4467
TheLeoP <eugenio2305@hotmail.com>
2025-03-21 13:36:09 -0500

    fix: add DevicesOnLocalNetworkProvider again

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -6,11 +6,14 @@ import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClient } from './util'
 import { RouterProvider } from '@tanstack/react-router'
 import { router } from './router'
+import { DevicesOnLocalNetworkProvider } from './components'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
+      <DevicesOnLocalNetworkProvider>
+        <RouterProvider router={router} />
+      </DevicesOnLocalNetworkProvider>
     </QueryClientProvider>
   </React.StrictMode>,
 )
