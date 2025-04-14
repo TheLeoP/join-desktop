5508c70f7f6c9e5fb425566b3f84dfd0e10c0f62
TheLeoP <eugenio2305@hotmail.com>
2025-04-14 15:13:50 -0500

    feat: slightly better UI for push history

--- src/renderer/src/routes/history.tsx
+++ src/renderer/src/routes/history.tsx
@@ -1,6 +1,7 @@
 import { useQuery } from '@tanstack/react-query'
 import { createFileRoute } from '@tanstack/react-router'
 import { zodValidator } from '@tanstack/zod-adapter'
+import { useRef, useEffect } from 'react'
 import { z } from 'zod'
 
 const searchSchema = z.object({
@@ -29,11 +30,25 @@ function RouteComponent() {
     queryKey: ['pushHistory', deviceId],
   })
 
+  const endOfList = useRef<HTMLDivElement | null>(null)
+  useEffect(() => {
+    endOfList.current?.scrollIntoView()
+  }, [history])
+
   if (isPending) {
     return <div>Loading...</div>
   } else if (isError) {
     return <div>Error: {error.message}</div>
   }
 
-  return <div>{JSON.stringify(history)}</div>
+  return (
+    <div className="ms-1 mt-1 flex h-[calc(100vh-49px)] flex-col space-y-1 space-x-1 overflow-y-auto">
+      {history.map((push, i) => (
+        <pre key={i} className="bg-orange-100">
+          {JSON.stringify(push, null, 2)}
+        </pre>
+      ))}
+      <div ref={endOfList}></div>
+    </div>
+  )
 }
